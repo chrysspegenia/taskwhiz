9c99addb461bbff676a813521656baeaa4a58a32
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-08-30 17:03:49 +0800

    Add setup to allow image src from localhost

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["localhost"], // Add other domains as needed
+  },
+};
 
 export default nextConfig;
