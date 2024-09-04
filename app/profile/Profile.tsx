ca533ace808f81eaf110c523a2174f9bbbeb0e1a
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-09-04 15:30:33 +0800

    Modified data fetch to use useQuery instead of useEffect

--- app/profile/Profile.tsx
+++ app/profile/Profile.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useState, useEffect } from "react";
 import Avatar from "@/components/Avatar";
 import { API_URL } from "@/lib/constants";
+import { useQuery } from "@tanstack/react-query";
 
 type ProfileData = {
   first_name: string;
@@ -16,61 +16,44 @@ export default function Profile({
   token: string;
   user_id: string;
 }) {
-  const [profileData, setProfileData] = useState<ProfileData | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  const { isPending, data, error, isError } = useQuery<ProfileData>({
+    queryKey: ["userData", user_id],
+    queryFn: async () => {
+      const response = await fetch(`${API_URL}/users/${user_id}`, {
+        method: "GET",
+        headers: { Authorization: token },
+      });
 
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const response = await fetch(`${API_URL}/users/${user_id}`, {
-          method: "GET",
-          headers: { Authorization: token },
-        });
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch profile data.");
-        }
-
-        const data = await response.json();
-        setProfileData({
-          first_name: data.first_name,
-          last_name: data.last_name,
-          avatar_url: data.avatar_url,
-        });
-      } catch (error: any) {
-        // Adjust type if using TypeScript
-        setError(error.message || "Error fetching profile data.");
-      } finally {
-        setLoading(false);
+      if (!response.ok) {
+        throw new Error("Failed to fetch profile data.");
       }
-    };
 
-    fetchProfile();
-  }, [token, user_id]);
+      return response.json();
+    },
+  });
 
-  if (loading) {
+  if (isPending) {
     return <p>Loading...</p>;
   }
 
-  if (error) {
-    return <p>Error: {error}</p>;
+  if (isError) {
+    return <p>Error: {(error as Error).message}</p>;
   }
 
   return (
     <main>
       <section>
         <h1>Profile</h1>
-        {profileData?.avatar_url && (
+        {data?.avatar_url && (
           <div>
-            <Avatar avatarUrl={profileData.avatar_url} />
+            <Avatar avatarUrl={data.avatar_url} />
           </div>
         )}
         <p>
-          <strong>First Name:</strong> {profileData?.first_name}
+          <strong>First Name:</strong> {data?.first_name}
         </p>
         <p>
-          <strong>Last Name:</strong> {profileData?.last_name}
+          <strong>Last Name:</strong> {data?.last_name}
         </p>
       </section>
     </main>
